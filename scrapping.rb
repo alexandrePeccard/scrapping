2485895cdc5c05b67b1b41958fe938f43deafbb3
Alexandre Peccard <alexandre.peccard@gmail.com>
2018-10-11 22:45:20 +0200

    fix somes issues

--- scrapping.rb
+++ scrapping.rb
@@ -1,24 +1,30 @@
 require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
+require 'time'
 
 class Parser
-	attr_accessor :nokogiri, :base_url
-
+	attr_accessor :base_url
+	
 	def initialize
-		@nokogiri = Nokogiri::HTML
 		@base_url = ""
 	end
 
 	def clean_url(url)
-		return "http://annuaire-des-mairies.com" + url.inner_html.split('.')[1] + ".html"
+		begin
+			url = [url.inner_html.split('.')[1].split('/')[2].capitalize, @base_url + url.inner_html.split('.')[1] + ".html"]
+		rescue StandardError => e
+			puts e.class
+			puts e.message
+		end
+
+		return url 
 	end
 
 	def get_the_email_of_a_townhal_from_its_webpage(url)
-		url = clean_url(url)
 		data = get_content(url)
 		email = ""
-		data.css('table tbody tr td').each do | line | 
+		data.css('table tbody tr td').each do | line |
 			begin
 				email = line.inner_html if /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.match?(line.inner_html)
 			rescue StandardError => e
@@ -37,19 +43,24 @@ class Parser
 	def get_all_the_urls_of_val_doise_townhalls(url)
 		data = get_content(url)
 		links = []
-		data.css('body table tr td .lientxt @href').each { | link | links << link }
+		begin
+			data.css('body table tr td .lientxt @href').each { | link | links << link }
+		rescue StandardError => e
+			puts e.class
+			puts e.message
+		end
 
 		return links
 	end
 
-	#Scrapping des villes
 	def scrapping_cities
-		base_url = 'http://annuaire-des-mairies.com'
+		@base_url = 'http://annuaire-des-mairies.com'
 		links = get_all_the_urls_of_val_doise_townhalls('http://annuaire-des-mairies.com/val-d-oise.html')
 		emails = []
 		links.each do | url |
+			city, url = clean_url(url)
 			email = get_the_email_of_a_townhal_from_its_webpage(url)
-			myhash = { "test" => email }
+			myhash = { city => email }
 			emails << myhash
 		end
 
@@ -57,7 +68,7 @@ class Parser
 	end
 
 	def scrapping_crypomonney
-		base_url = 'https://coinmarketcap.com/all/views/all/'
+		@base_url = 'https://coinmarketcap.com/all/views/all/'
 		data = get_content(base_url)
 		values = []
 		data.css('table tr[id*="id-"]').each do |line|
@@ -65,38 +76,68 @@ class Parser
 			brand = line.css('td')[1].css('a')[1].inner_html
 			values << { brand => price }
 		end
+
+		puts values
+		puts Time.now
+
 		return values
 	end
 
-	def scrapping_deputes
-		base_url = "https://wiki.laquadrature.net/Contactez_vos_d%C3%A9put%C3%A9s"
-		data = Nokogiri::HTML(open(base_url))
-		members = []
+	def scrap_deputes
+		@base_url = "https://www.nosdeputes.fr/deputes"
+		data = get_content(@base_url)
+		links = []
 		begin
-			data.css('#mw-content-text ul li').each do | line |
+			lines = data.css('tr td a @href')
+			lines.each do |line|
+				links << @base_url.split('/deputes')[0] + line.inner_html
+			end
 
-				line.inner_html.split(" ").each do |tocheck|
-					if /\w/.match?(tocheck)
-						puts tocheck
-						# return false
+			deputes = {}
+			links.each do | url |
+				begin
+				data = get_content(url)
+				nom = data.css('div .info_depute h1').inner_html
+				puts nom
+				mail =  data.css('div[id="b1"] ul')[2].css('li a @href').inner_html.split("mailto:")
+				deputes[nom] = []
+				mail.each do |m|
+					if m.size > 0
+						deputes[nom] << m
+						puts m
 					end
-					# puts "#{line[0]}#{line[1]}"
-				# 	puts tocheck
-				# 	# /<li>/
-				# 	# return false
-				# 	members << tocheck if /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.match?(tocheck)
+				end
+				rescue StandardError => e
+					puts e.class
+					puts e.message
 				end
 			end
-			return false
+			puts deputes
 		rescue StandardError => e
 			puts e.class
 			puts e.message
 		end
-
-		return members
 	end
 end
-parser = Parser.new
-# parser.scrapping_cities
-# puts parser.scrapping_crypomonney
-parser.scrapping_deputes
+
+def run
+	 #Instanciation de l'objet Parser
+	parser = Parser.new
+
+	# Appel de la méthode scrapping_cities pour afficher les villes et leurs mails 
+	
+	# parser.scrapping_cities
+
+	# Appel de la méthode scrapping_crypomonney pour récupérer les noms et valeurs des cryptomonnaies 	
+	# Dans une boucle infinie pour relancer le script toutes les 15 secondes
+	
+	# while true
+	# 	parser.scrapping_crypomonney
+	# 	sleep(15)
+	# end
+
+	# Appel de la méthode scrapping_deputes pour récupérer les noms et adresses mails des députés 
+	parser.scrap_deputes
+end
+
+run
